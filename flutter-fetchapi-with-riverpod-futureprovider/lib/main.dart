be07afa8944ee5b859e40a37b6099ae38eb28dd8
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-11-19 19:07:57 -0300

    Novo Teste para um Artigo

---
+++ flutter-fetchapi-with-riverpod-futureprovider/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:api_future/screens/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          useMaterial3: false,
+          primarySwatch: Colors.blue,
+        ),
+        home: const Homepage());
+  }
+}
---
+++ flutter-fetchapi-with-riverpod-futureprovider/lib/services/user_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:api_future/model/user_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart';
+
+class ApiService {
+  String endpoint = 'https://reqres.in/api/users?page=2';
+
+  Future<List<UserModel>> getUser() async {
+    try {
+      Response response = await get(Uri.parse(endpoint));
+      if (response.statusCode == 200) {
+        final List result = jsonDecode(response.body)['data'];
+        return result.map(((e) => UserModel.fromJson(e))).toList();
+      } else {
+        throw Exception(response.reasonPhrase);
+      }
+    } on SocketException catch (error) {
+      throw Exception('No  internet connection');
+    }
+  }
+}
+
+//API SERVICE Provider
+final apiProvider = Provider<ApiService>((ref) => ApiService());
